d01e7391974d2f0d7694bb8157927fea2c6ef251
Ryan <ryanevans253@yahoo.com>
2022-02-01 18:44:41 -0800

    Chart.js plugged in

--- src/components/flipChart/FlipChart.js
+++ src/components/flipChart/FlipChart.js
@@ -3,11 +3,39 @@ import "chart.js/auto";
 import { Doughnut } from "react-chartjs-2";
 
 export default function FlipChart() {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: false,
+        text: "Fix and Flip Expenses",
+        fontColor: "rgba(95, 91, 255, 1)",
+      },
+    },
+  };
+
   const data = {
-    labels: ["red", "blue", "yellow", "green"],
+    labels: [
+      "Purchase Closing Costs",
+      "Sale Closing Costs",
+      "Repair Costs",
+      "Sales Commission",
+      "Holding Cost",
+    ],
+
     datasets: [
       {
-        data: [12, 15, 30, 16],
+        data: [12, 15, 30, 16, 20],
+        backgroundColor: [
+          "rgba(97,245,245)",
+          "rgba(98,195,245)",
+          "rgba(97,147,245)",
+          "rgba(98,96,244)",
+          "rgba(99,49,245)",
+        ],
       },
     ],
   };
@@ -15,7 +43,7 @@ export default function FlipChart() {
   return (
     <div>
       <h1>Donut chart</h1>
-      <Doughnut data={data} />
+      <Doughnut data={data} options={options} />
     </div>
   );
 }
